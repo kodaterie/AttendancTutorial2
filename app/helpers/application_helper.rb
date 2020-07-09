23dabd897c1c182148eddac18848cb66953a1fd5
kodaterie <kre2828kre@gmail.com>
2020-07-09 15:29:19 +0000

    Finish top-page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,12 @@
 module ApplicationHelper
   
-  def full_title(page_name = "")
-    base_title = "AttendanceApp"
-    if page_name.empty?
-      base_title
-    else
-      page_name + " | " + base_title
+  # ページごとにタイトルを返す
+  def full_title(page_name = "")　# メソッドと因数の定義
+    base_title = "AttendanceApp"  # 基本となるアプリケーション名を変数に代入
+    if page_name.empty?　# 因数を受け取っているか判定
+      base_title  # 因数page_nameが空文字の場合はbase_titleのみ返す
+    else　 # 因数page_nameが空文字ではない場合
+      page_name + " | " + base_title # 文字列を連結して返す
     end
   end
 end
